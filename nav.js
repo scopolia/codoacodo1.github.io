fdf397acd5b181b58185923206ede6281b04693a
scopolia <126039257+scopolia@users.noreply.github.com>
2023-04-24 23:55:03 -0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,12 @@
+const toggleMenuElement = document.getElementById('toggle-menu');
+const mainMenuElement = document.getElementById('main-menu');
+
+toggleMenuElement.addEventListener('click', ()=>{
+    mainMenuElement.classList.toggle('main-menu--show')
+   
+
+});
+
+
+
+
